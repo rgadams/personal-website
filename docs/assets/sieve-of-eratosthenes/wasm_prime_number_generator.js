d65d70f3ba5788766f5c2091b0d5a37b424dc8d9
Roy Adams <roy.g.adams@gmail.com>
2021-07-16 10:41:34 -0400

    Committing new build

---
+++ docs/assets/sieve-of-eratosthenes/wasm_prime_number_generator.js
@@ -0,0 +1,2 @@
+import * as wasm from "./wasm_prime_number_generator_bg.wasm";
+export * from "./wasm_prime_number_generator_bg.js";
---
+++ docs/assets/wasm-gravity/gravity_wasm.js
@@ -0,0 +1,2 @@
+import * as wasm from "./gravity_wasm_bg.wasm";
+export * from "./gravity_wasm_bg.js";
